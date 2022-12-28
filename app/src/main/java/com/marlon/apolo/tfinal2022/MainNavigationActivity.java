c5da78279fade7cd10b7ec1232251e2c2417c99b
norl4m <4ndroidd3v3l@gmail.com>
2022-12-28 09:29:55 -0500

    Actualizando a la version final

--- app/src/main/java/com/marlon/apolo/tfinal2022/MainNavigationActivity.java
+++ app/src/main/java/com/marlon/apolo/tfinal2022/MainNavigationActivity.java
@@ -120,7 +120,7 @@ public class MainNavigationActivity extends AppCompatActivity {
                         if (cita.getTo().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
                             if (cita.isStateReceive() && cita.isState() && cita.getCalificacion() == 0) {
                                 Log.d(TAG, "#################################");
-                                Log.d(TAG, "LOKURA");
+//                                Log.d(TAG, "LOKURA");
                                 alertD = califTrabajador(cita);
                                 try {
                                     alertD.show();
